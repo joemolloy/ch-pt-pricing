96af7413019df12133a199f1a11d69a516ac8fe2
clivings <clarissa.livingston@ivt.baug.ethz.ch>
2020-08-26 18:04:35 +0200

    install_glpk.sh was modified to work better on nas

--- install_glpk.sh
+++ install_glpk.sh
@@ -3,14 +3,14 @@
 ### If it should be installed on the server for all to use, it can be easily modified.
 
 # Install directory
-INSTALL_DIRECTORY=/home/$USER/glpk
+INSTALL_DIRECTORY=/nas/$USER/glpk
 
 # Set JAVA_HOME
-export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac)))
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
 
 # Download source code
-mkdir -p /home/$USER/src
-cd /home/$USER/src
+mkdir -p /nas/$USER/src
+cd /nas/$USER/src
 rm -rf glpk-glpk_4_65*
 wget http://ftp.gnu.org/gnu/glpk/glpk-4.65.tar.gz
 tar -xzf glpk-4.65.tar.gz
@@ -20,14 +20,14 @@ glpk-java/glpk-java-1.12.0/libglpk-java-1.12.0.tar.gz
 tar -xzf libglpk-java-1.12.0.tar.gz
 
 # Build and install GLPK
-cd /home/$USER/src/glpk-4.65
+cd /nas/$USER/src/glpk-4.65
 ./configure --prefix=$INSTALL_DIRECTORY
 make -j6
 make check
 make install
 
 # Build and install GLPK for Java
-cd /home/$USER/src/libglpk-java-1.12.0
+cd /nas/$USER/src/libglpk-java-1.12.0
 export CPPFLAGS=-I$INSTALL_DIRECTORY/include
 export SWIGFLAGS=-I$INSTALL_DIRECTORY/include
 export LD_LIBRARY_PATH=$INSTALL_DIRECTORY/lib
